d38ad9fc92728fb0223ddc22f4c4abdc439d9a0b
Long.VK <long.vk@kiotviet.com>
2024-12-03 16:19:17 +0700

    another commit

--- st-app.py
+++ st-app.py
@@ -10,25 +10,37 @@ import joblib
 import json
 from validator import validate_date, validate_price, validate_warehouse_capacity, validate_truck_capacity
 
-# from keras.api.models import load_model
+from keras.models import load_model
 
 connection = pymysql.connect(
     host='localhost',
     user='root',
-    password='juggernautlong2003',
+    password='Liquid@123',
     database='prj3',
     cursorclass=pymysql.cursors.DictCursor
 )
 
 st.set_page_config("Warehouse Forecasting")
 
-# @st.cache_resource
-# def load_my_model():
-#     return load_model("final.h5")
-#
-#
-# model = load_my_model()
-# sc = joblib.load("scaler.pkl")
+@st.cache_resource
+def load_lstm_model():
+    return load_model("model_lstm.h5")
+
+
+@st.cache_resource
+def load_rnn_model():
+    return load_model("model_rnn.h5")
+
+
+@st.cache_resource
+def load_gru_model():
+    return load_model("model_gru.h5")
+
+
+model_lstm = load_lstm_model()
+model_rnn = load_rnn_model()
+model_gru = load_gru_model()
+sc = joblib.load("scaler.pkl")
 
 columns = ['price', 'warehouse_capacity', 'truck_capacity', 'date']
 
@@ -109,10 +121,14 @@ def add_row():
             st.rerun()
 
 
-def visualize(df):
+def visualize(df, df_pred=None):
     fig, ax = plt.subplots(figsize=(10, 6))
     ax.plot(df['date'], df['warehouse_capacity'], label='warehouse_capacity', marker='o')
     ax.plot(df['date'], df['truck_capacity'], label='truck_capacity', marker='x')
+
+    if df_pred is not None:
+        ax.plot(df_pred['date'], df_pred['warehouse_capacity'], label='warehouse_capacity_prediction', marker='o')
+        ax.plot(df_pred['date'], df_pred['truck_capacity'], label='truck_capacity_prediction', marker='x')
 
     # Adding labels and title
     ax.set_xlabel('Date')
@@ -125,8 +141,46 @@ def visualize(df):
     return fig
 
 
+def predict(df, model, scaler: MinMaxScaler, cur_date):
+    scaled_df = scaler.transform(df.drop(['id', 'date'], axis=1))
+    scaled_df = scaled_df[-14:, :]
+
+    # X = []
+    # for i in range(30):
+    #     X.append(scaled_df[])
+    X = [scaled_df]
+    X = np.array(X)
+
+    predictions = []
+    for _ in range(14):  # Predict for the next 14 days
+        next_pred = model.predict(X[-30:, :, :])
+        predictions.append(sc.inverse_transform(next_pred))
+        # Update the sequence with the new prediction
+        X = np.append([next_pred], X, axis=1)
+
+    data = {
+        'date': [],
+        'price': [],
+        'warehouse_capacity': [],
+        'truck_capacity': []
+    }
+
+    for i in range(14):
+        data['date'].append(cur_date)
+        data['price'].append(int(predictions[i][0][0]))
+        data['warehouse_capacity'].append(int(predictions[i][0][1]))
+        data['truck_capacity'].append(int(predictions[i][0][2]))
+        cur_date = cur_date + pd.Timedelta(days=1)
+
+    return pd.DataFrame(data)
+
+
 def toggle_figure():
     st.session_state.visualize = not st.session_state.visualize
+
+
+def toggle_predict():
+    st.session_state.predict = not st.session_state.predict
 
 
 def page_1():
@@ -152,9 +206,35 @@ def page_1():
         st.session_state.figure = fig
 
         st.write('### Visualize:')
-        st.pyplot(fig)
+        st.pyplot(st.session_state.figure)
 
         # st.rerun()
+
+    if st.button("Predict"):
+        toggle_predict()
+
+    current_date = pd.to_datetime(st.session_state.df['date']).max()
+    if st.session_state.predict:
+        df_pred_lstm = predict(st.session_state.df, model_lstm, sc , cur_date=current_date)
+        df_pred_rnn = predict(st.session_state.df, model_rnn, sc,  cur_date=current_date)
+        df_pred_gru = predict(st.session_state.df, model_gru, sc,  cur_date=current_date)
+
+        lstm_fig = visualize(st.session_state.df, df_pred_lstm)
+        rnn_fig = visualize(st.session_state.df, df_pred_rnn)
+        gru_fig = visualize(st.session_state.df, df_pred_gru)
+
+        st.session_state.lstm_fig = lstm_fig
+        st.session_state.rnn_fig = rnn_fig
+        st.session_state.gru_fig = gru_fig
+
+        st.write("### Prediction using LSTM model")
+        st.pyplot(st.session_state.lstm_fig)
+
+        st.write("### Prediction using RNN model")
+        st.pyplot(st.session_state.rnn_fig)
+
+        st.write("### Prediction using GRU model")
+        st.pyplot(st.session_state.gru_fig)
 
 
 def page_2():
@@ -181,6 +261,9 @@ def main():
 
     if 'visualize' not in st.session_state:
         st.session_state.visualize = False
+
+    if 'predict' not in st.session_state:
+        st.session_state.predict = False
 
     # st.sidebar.title("Navigation")
     # choice = st.sidebar.selectbox("Select an option", list(PAGES.keys()))
