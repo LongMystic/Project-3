d24a00c1926d5b875701ed75e66367efa8698e1c
Long.VK <long.vk@kiotviet.com>
2024-12-31 18:21:50 +0700

    modify st-app

--- st-app.py
+++ st-app.py
@@ -45,88 +45,12 @@ model_lstm = load_lstm_model()
 model_rnn = load_rnn_model()
 model_gru = load_gru_model()
 
-columns = ['price', 'warehouse_capacity', 'truck_capacity', 'date']
+columns = ['Unit quantity', 'Weight', 'Truck Count', 'Order Date']
 drop_columns = ['Order ID', 'Origin Port', 'Plant ID', 'Daily Capacity ', 'Plant Code', 'Destination Port', 'Carrier', 'Customer', 'Service Level'] # 9
 numeric_columns = ['TPT', 'Ship ahead day count', 'Ship Late Day count', 'Product ID', 'Unit quantity', 'Weight', 'Truck Count'] # 7
 
 
-def insert_row_to_db():
-    sql = f"""
-        INSERT INTO prj3.data (date, price, warehouse_capacity, truck_capacity)
-        VALUE (
-            {st.session_state.date}
-            , {st.session_state.price}
-            , {st.session_state.warehouse_capacity}
-            , {st.session_state.truck_capacity}
-        );
-    """
-    cursor = connection.cursor()
-    cursor.execute(sql)
-    connection.commit()
-
-    return 1
-
-
-def fetch_data():
-    sql = """
-        SELECT * FROM prj3.data;
-    """
-    cursor = connection.cursor()
-    cursor.execute(sql)
-    rows = cursor.fetchall()
-
-    columns_df = [desc[0] for desc in cursor.description]
-
-    df = pd.DataFrame(rows, columns=columns_df)
-
-    return df
-
-
-def add_row():
-    st.write("### Add New Row")  # Heading for modal
-    st.session_state.date = st.text_input("Enter Date")
-    st.session_state.price = st.text_input("Enter Price")
-    st.session_state.warehouse_capacity = st.text_input("Enter Warehouse Capacity")
-    st.session_state.truck_capacity = st.text_input("Enter Truck Capacity")
-
-    # Save and Cancel buttons within modal
-    col1, col2 = st.columns(2)
-    with col1:
-        if st.button("Save"):
-            # validate data
-            code, message = validate_date(st.session_state.date)
-            if code == -1:
-                st.error(message)
-                return
-
-            code, message = validate_price(st.session_state.price)
-            if code == -1:
-                st.error(message)
-                return
-
-            code, message = validate_warehouse_capacity(st.session_state.warehouse_capacity)
-            if code == -1:
-                st.error(message)
-                return
-
-            code, message = validate_truck_capacity(st.session_state.truck_capacity)
-            if code == -1:
-                st.error(message)
-                return
-
-            if code == 0:
-                # insert_row_to_db()
-                st.info("Add row successfully!")
-                time.sleep(2)
-                st.session_state.add_row = False
-                st.rerun()  # Refresh the app to display the updated DataFrame
-    with col2:
-        if st.button("Cancel"):
-            st.session_state.add_row = False  # Close modal without saving
-            st.rerun()
-
-
-def visualize_with_ex(df, df_pred=None):
+def visualize_with_ex(df, column_name, df_pred=None):
     df = df.tail(30)
 
     # Create the base figure
@@ -135,9 +59,9 @@ def visualize_with_ex(df, df_pred=None):
     # Add lines for warehouse and truck capacity
     fig.add_trace(go.Scatter(
         x=df['Order Date'],
-        y=df['Unit quantity'],
+        y=df[column_name],
         mode='lines+markers',
-        name='Unit quantity',
+        name=column_name,
         marker=dict(symbol='circle')
     ))
 
@@ -146,42 +70,22 @@ def visualize_with_ex(df, df_pred=None):
     if df_pred is not None:
         fig.add_trace(go.Scatter(
             x=df_pred['Order Date'],
-            y=df_pred['Unit quantity'],
+            y=df_pred[column_name],
             mode='lines+markers',
-            name='Unit quantity_prediction',
+            name=f'{column_name} prediction',
             marker=dict(symbol='circle')
         ))
 
 
     # Update layout for better appearance
     fig.update_layout(
-        title='Capacity Over Time',
+        title=f'{column_name} Over Time',
         xaxis_title='Date',
         yaxis_title='Value',
         legend_title='Legend',
         xaxis=dict(tickangle=45),
         template='plotly_white'
     )
-
-    return fig
-
-def visualize(df, df_pred=None):
-    df = df.tail(30)
-    fig, ax = plt.subplots(figsize=(10, 6))
-    ax.plot(df['Order Date'], df['warehouse_capacity'], label='warehouse_capacity', marker='o')
-    ax.plot(df['Order Date'], df['truck_capacity'], label='truck_capacity', marker='x')
-
-    if df_pred is not None:
-        ax.plot(df_pred['Order Date'], df_pred['warehouse_capacity'], label='warehouse_capacity_prediction', marker='o')
-        ax.plot(df_pred['Order Date'], df_pred['truck_capacity'], label='truck_capacity_prediction', marker='x')
-
-    # Adding labels and title
-    ax.set_xlabel('Order Date')
-    ax.set_ylabel('Value')
-    ax.set_title('Capacity Over Time')
-    ax.legend()
-    plt.xticks(rotation=45)
-    plt.tight_layout()
 
     return fig
 
@@ -211,13 +115,16 @@ def predict(_df, model):
     df.set_index('Order Date', inplace=True)
     df = df[numeric_columns]
 
-    average_last_30 = df[['Unit quantity']].iloc[-30:].mean()
+    average_last_30 = df[['Unit quantity', 'Weight', 'Truck Count']].iloc[-30:].mean()
 
-    average_last_5 = df[['Unit quantity']].iloc[-5:].mean()
+    average_last_5 = df[['Unit quantity', 'Weight', 'Truck Count']].iloc[-5:].mean()
 
-    average_warehouse_last_30 = average_last_30['Unit quantity']
-
-    average_warehouse_last_5 = average_last_5['Unit quantity']
+    unit_quantity_last_30 = average_last_30['Unit quantity']
+    unit_quantity_last_5 = average_last_5['Unit quantity']
+    weight_last_30 = average_last_5['Weight']
+    weight_last_5 = average_last_5['Weight']
+    truck_count_last_30 = average_last_5['Truck Count']
+    truck_count_last_5 = average_last_5['Truck Count']
 
 
     scaler = MinMaxScaler()
@@ -225,7 +132,7 @@ def predict(_df, model):
     last_sequence = scaled_data[-5:]
     predicted = []
 
-    predict_days = 14
+    predict_days = 7
     for _ in range(predict_days):
         prediction = model.predict(last_sequence[np.newaxis, :, :])
         predicted.append(prediction[0])
@@ -236,13 +143,23 @@ def predict(_df, model):
     predicted_df = pd.DataFrame(predicted, columns=df.columns)
     predicted_df.index = pd.date_range(start=df.index[-1] + pd.Timedelta(days=1), periods=predict_days)
     predicted_df['Unit quantity'] = predicted_df['Unit quantity'].astype(int)
+    predicted_df['Weight'] = predicted_df['Weight'].astype(float)
+    predicted_df['Truck Count'] = predicted_df['Truck Count'].astype(int)
     predicted_df = predicted_df.reset_index()
     predicted_df.rename(columns={'index': 'Order Date'}, inplace=True)
-    warehouse_evaluation = []
+    unit_quantity_evaluation = []
+    weight_evaluation = []
+    truck_count_evaluation = []
     for i in range(len(predicted_df)):
-        warehouse_evaluation.append(evaluate(average_warehouse_last_30, average_warehouse_last_5,
+        unit_quantity_evaluation.append(evaluate(unit_quantity_last_30, unit_quantity_last_5,
                                              predicted_df.iloc[i]['Unit quantity']))
-    predicted_df['Unit quantity_evaluation'] = warehouse_evaluation
+        weight_evaluation.append(evaluate(weight_last_30, weight_last_5,
+                                                 predicted_df.iloc[i]['Weight']))
+        truck_count_evaluation.append(evaluate(truck_count_last_30, truck_count_last_5,
+                                                 predicted_df.iloc[i]['Truck Count']))
+    predicted_df['Unit quantity evaluation'] = unit_quantity_evaluation
+    predicted_df['Weight evaluation'] = weight_evaluation
+    predicted_df['Truck Count evaluation'] = truck_count_evaluation
     return predicted_df
 
 
@@ -283,12 +200,6 @@ def page_1():
 
     col1, col2 = st.columns(2)
     with col1:
-        if st.button("Add row"):
-            st.session_state.add_row = True
-
-        if st.session_state.get("add_row", False):
-            add_row()
-    with col2:
         if st.button("Upload a csv file"):
             st.session_state.upload_file = True
         if st.session_state.get("upload_file", False):
@@ -300,15 +211,22 @@ def page_1():
             toggle_figure()
 
         if st.session_state.visualize:
-            fig = visualize_with_ex(st.session_state.df)
+            fig_uq = visualize_with_ex(st.session_state.df, "Unit quantity")
+            fig_w = visualize_with_ex(st.session_state.df, "Weight")
+            fig_tc = visualize_with_ex(st.session_state.df, "Truck Count")
 
-            st.session_state.figure = fig
+            st.session_state.fig_uq = fig_uq
+            st.session_state.fig_w = fig_w
+            st.session_state.fig_tc = fig_tc
 
             st.write('### Visualize:')
-            st.plotly_chart(st.session_state.figure)
+            st.plotly_chart(st.session_state.fig_uq)
+            st.plotly_chart(st.session_state.fig_w)
+            st.plotly_chart(st.session_state.fig_tc)
 
             # st.rerun()
 
+    col1, col2, col3 = st.columns(3)
     with col1:
         model_name = st.selectbox('Choose a model:', ['RNN', 'LSTM', 'GRU'])
 
@@ -317,58 +235,51 @@ def page_1():
             toggle_predict()
 
     if st.session_state.predict:
-        df_pred_lstm = predict(st.session_state.df, model_lstm)
-        df_pred_rnn = predict(st.session_state.df, model_rnn)
-        df_pred_gru = predict(st.session_state.df, model_gru)
+        if model_name == "RNN":
+            model_predict = model_rnn
+        elif model_name == "LSTM":
+            model_predict = model_lstm
+        else:
+            model_predict = model_gru
 
-        lstm_fig = visualize_with_ex(st.session_state.df, df_pred_lstm)
-        rnn_fig = visualize_with_ex(st.session_state.df, df_pred_rnn)
-        gru_fig = visualize_with_ex(st.session_state.df, df_pred_gru)
+        df_pred = predict(st.session_state.df, model_predict)
 
-        st.session_state.lstm_fig = lstm_fig
-        st.session_state.rnn_fig = rnn_fig
-        st.session_state.gru_fig = gru_fig
+        pred_fig_unit_quantity = visualize_with_ex(st.session_state.df, "Unit quantity", df_pred)
+        pred_fig_weight = visualize_with_ex(st.session_state.df, "Weight", df_pred)
+        pred_fig_truck_count = visualize_with_ex(st.session_state.df, "Truck Count", df_pred)
 
-        col1, col2 = st.columns(2)
-        with col1:
-            st.write("### Prediction using LSTM model")
-            st.plotly_chart(st.session_state.lstm_fig)
+        st.session_state.pred_fig_unit_quantity = pred_fig_unit_quantity
+        st.session_state.pred_fig_weight = pred_fig_weight
+        st.session_state.pred_fig_truck_count = pred_fig_truck_count
 
-        with col2:
-            st.dataframe(df_pred_lstm[['Order Date', 'Unit quantity', 'Unit quantity_evaluation']])
-
-        col1, col2 = st.columns(2)
-        with col1:
-            st.write("### Prediction using RNN model")
-            st.plotly_chart(st.session_state.rnn_fig)
-
-        with col2:
-            st.dataframe(df_pred_rnn[['Order Date', 'Unit quantity', 'Unit quantity_evaluation']])
+        st.write(f"### Prediction using {model_name} model")
 
         col1, col2 = st.columns(2)
         with col1:
-            st.write("### Prediction using GRU model")
-            st.plotly_chart(st.session_state.gru_fig)
+            st.write("### Prediction about unit quantity")
+            st.plotly_chart(st.session_state.pred_fig_unit_quantity)
 
         with col2:
-            st.dataframe(df_pred_gru[['Order Date', 'Unit quantity', 'Unit quantity_evaluation']])
+            st.dataframe(df_pred[['Order Date', 'Unit quantity', 'Unit quantity evaluation']])
 
+        col1, col2 = st.columns(2)
+        with col1:
+            st.write("### Prediction about weight")
+            st.plotly_chart(st.session_state.pred_fig_weight)
 
-def page_2():
-    pass
+        with col2:
+            st.dataframe(df_pred[['Order Date', 'Unit quantity', 'Weight evaluation']])
 
+        col1, col2 = st.columns(2)
+        with col1:
+            st.write("### Prediction about truck count")
+            st.plotly_chart(st.session_state.pred_fig_truck_count)
 
-# PAGES = {
-#     "Visualization": page_1,
-#     "Data": page_2
-# }
-
+        with col2:
+            st.dataframe(df_pred[['Order Date', 'Unit quantity', 'Truck Count evaluation']])
 
 def main():
     if 'df' not in st.session_state:
-        # df = pd.DataFrame(columns=columns)
-        # df.set_index("date", inplace=True)
-        # st.session_state.df = fetch_data()
         st.session_state.df = pd.read_csv('./data/cleaned_data.csv')
 
     if 'page_2_df' not in st.session_state:
@@ -386,10 +297,6 @@ def main():
     if 'upload_file' not in st.session_state:
         st.session_state.upload_file = False
 
-    # st.sidebar.title("Navigation")
-    # choice = st.sidebar.selectbox("Select an option", list(PAGES.keys()))
-    #
-    # PAGES[choice]()
     page_1()
 
 
